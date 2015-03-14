22481c5087fcb5c87fceb6f9cb8b71d39b847edf
kousuke.kurihara <xmx1fc@gmail.com>
2015-03-14 23:50:08 +0900

    initialize raisl app

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'be7f70609b23e319e85e7b72c02ffce4ec91e32cebcf323db82865c4ba4ca2713ac7d0925f52895834e1483cc2bd4cca522a2d19466b0d187004a0c3d70db912'
